6df28cba807c453dad477fee9d3b1cc4cc3d800c
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-07-12 17:42:50 +0200

    Import de la bruteforce

---
+++ optimized/optimized.py
@@ -0,0 +1,9 @@
+# import sys
+# sys.path.append(r"C:\Users\Ciran\Desktop\OC - Python\Projet n°7 - Résolvez des problèmes en utilisant des algorithmes en Python\Production\oc-cg-p7")
+
+from bruteforce.bruteforce import Action
+
+actiontest = Action("action test", 2.5, 3.3)
+
+print(actiontest)
+
